6cee938fbbb97a048d2abc27da0dc93d75131084
Edryan López <ealopezla@correo.url.edu.gt>
2025-05-11 23:41:58 -0600

    Creacion basica de chat

---
+++ SeminarioIA/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeminarioIA.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
