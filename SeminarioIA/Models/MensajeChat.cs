cc6e01d3d0679dbda6d160932cf422fc6f3028fa
Edryan López <ealopezla@correo.url.edu.gt>
2025-05-13 21:22:27 -0600

    Creación de modelos para los mensajes y perfección de vistas

---
+++ SeminarioIA/Models/MensajeChat.cs
@@ -0,0 +1,9 @@
+﻿namespace SeminarioIA.Models
+{
+    public class MensajeChat
+    {
+        public string Texto { get; set; }
+        public string Emocion { get; set; }
+        public string Response { get; set; }
+    }
+}
